9980e42aba02c2a2386c06620c96e592d929da56
Pavel Tatarsky <vintik@mydomain.com>
2010-06-02 05:36:55 -0400

    fixed for mongoid better support

--- lib/declarative_authorization.rb
+++ lib/declarative_authorization.rb
@@ -1,8 +1,6 @@
 require File.join(%w{declarative_authorization rails_legacy})
 require File.join(%w{declarative_authorization helper})
 require File.join(%w{declarative_authorization in_controller})
-require File.join(%w{declarative_authorization in_model})
-require File.join(%w{declarative_authorization obligation_scope})
 
 min_rails_version = "2.1.0"
 if Rails::VERSION::STRING < min_rails_version
@@ -14,4 +12,8 @@ require File.join(%w{declarative_authorization railsengine}) if defined?(::Rails
 ActionController::Base.send :include, Authorization::AuthorizationInController
 ActionController::Base.helper Authorization::AuthorizationHelper
 
-ActiveRecord::Base.send :include, Authorization::AuthorizationInModel
+if defined? ActiveRecord
+  require File.join(%w{declarative_authorization in_model})
+  require File.join(%w{declarative_authorization obligation_scope})
+  ActiveRecord::Base.send :include, Authorization::AuthorizationInModel
+end
